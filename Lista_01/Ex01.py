ac70e63dcda9382229fcc3dd71bb51149de25ff6
Maria Clara Batista Viana Silva <viana.batista@escolar.ifrn.edu.br>
2026-09-07 23:24:44 +0000

    Renomeação de arquivos

--- Lista_01/Ex01.py
+++ Lista_01/Ex01.py
@@ -1,2 +1,11 @@
-nome=input("Digite seu nome: ")
-print("Olá, "+ nome)
+impar=[]
+par=[]
+print("Digite quatro valores inteiros")
+for i in range(4):
+    valor=int(input())
+    if valor%2==0:
+        par.append(valor)
+    else:
+        impar.append(valor)
+print("Soma dos pares =",sum(par))
+print("Soma dos ímpares =",sum(impar))
--- Testes_Iniciais/Ex01CS/obj/Debug/net10.0/Ex01CS.AssemblyInfo.cs
+++ Testes_Iniciais/Ex01CS/obj/Debug/net10.0/Ex01CS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex01CS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6763db0dd35911fc0b01e2da12db9f64ba48a91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec97f18e236ef20a2c27ac89dae3021ca222c52c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex01CS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex01CS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
